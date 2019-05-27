d23e1019e941789780fd4f5daf9a2a1b91d58bff
Kyle Murray <13399000+MagicAardvark@users.noreply.github.com>
2019-05-27 19:10:55 -0400

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,36 +1,35 @@
 const dev = {
   s3: {
-    REGION: 'us-east-1',
-    BUCKET: 'trade-tracker-api-dev-attachmentsbucket-1lpnn3rxpkq8r',
+    REGION: "YOUR_DEV_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_DEV_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: 'us-east-1',
-    URL: 'https://200a846kke.execute-api.us-east-1.amazonaws.com/dev',
+    REGION: "YOUR_DEV_API_GATEWAY_REGION",
+    URL: "YOUR_DEV_API_GATEWAY_URL"
   },
   cognito: {
-    REGION: 'us-east-1',
-    USER_POOL_ID: 'us-east-1_69GmK7uSY',
-    APP_CLIENT_ID: '3b2dh5vnu9umjn6n600fr0nlsv',
-    IDENTITY_POOL_ID: 'us-east-1:0aa51440-f8d4-4039-ab46-608c7daed463',
-  },
+    REGION: "YOUR_DEV_COGNITO_REGION",
+    USER_POOL_ID: "YOUR_DEV_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_DEV_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_DEV_IDENTITY_POOL_ID"
+  }
 };
 
 const prod = {
   s3: {
-    REGION: 'us-east-1',
-    BUCKET: 'trades-app-2-api-prod-serverlessdeploymentbucket-1vn6fit4lfnsa',
+    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: 'us-east-1',
-    URL: 'https://be7e2l9ay3.execute-api.us-east-1.amazonaws.com/prod',
+    REGION: "YOUR_PROD_API_GATEWAY_REGION",
+    URL: "YOUR_PROD_API_GATEWAY_URL"
   },
   cognito: {
-    REGION: 'us-east-1',
-    USER_POOL_ID: 'us-east-1_ldC5nVMMz',
-    APP_CLIENT_ID: '7eqg9e32o6r0dhs46mkij151u0',
-    IDENTITY_POOL_ID: 'us-east-1:b1620a17-cc7a-413e-817f-b40ebfb44fd2',
-  },
-  STRIPE_KEY: 'YOUR_STRIPE_PROD_PUBLIC_KEY',
+    REGION: "YOUR_PROD_COGNITO_REGION",
+    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+  }
 };
 
 // Default to dev if not set
@@ -41,5 +40,5 @@ const config = process.env.REACT_APP_STAGE === 'prod'
 export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
-  ...config,
+  ...config
 };
